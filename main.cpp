29609bdbae4834284deb33f949ea9258008a4d8d
champost <champak.br@gmail.com>
2016-11-22 10:35:41 -0500

    Clarifies the default number of local/global search points

--- main.cpp
+++ main.cpp
@@ -1103,13 +1103,12 @@ int main(int argc, char* argv[]) {
 				printf("Using the DIRECT_NOSCAL algorithm (i.e. without scaling) for the global search...\n");
 			}
 
-//			int globalMaxEvals = 1000 * tbiMsCmdIdx.size() * tbiMsCmdIdx.size();
-			int globalMaxEvals = 5000 * tbiMsCmdIdx.size();
-			if (!skipGlobalSearch && (globalEvals < globalMaxEvals)) {
+			int globalRecEvals = 2000 * tbiMsCmdIdx.size();
+			if (globalEvals < globalRecEvals) {
 				if (globalEvals)
-					printf("\nToo few global_search_points for the specified number of free parameters\nPlease consider increasing \"global_search_evals\"...\n");
+					printf("\nThe specified number of GLOBAL search points with respect to the number of \"tbi\" parameters is below the recommended value.\nPlease consider increasing \"global_search_evals\"...\n");
 				else
-					globalEvals = globalMaxEvals;
+					globalEvals = globalRecEvals;
 			}
 
 			printf("\nStarting global search...\n");
@@ -1167,22 +1166,27 @@ int main(int argc, char* argv[]) {
 				printf("\nUsing the user-specified/default values as a starting point for a local search...\n\n");
 			}
 
+			int localRecEvals = 1000 * tbiMsCmdIdx.size();
+			if (localEvals < localRecEvals) {
+				if (localEvals)
+					printf("\nThe specified number of LOCAL search points with respect to the number of \"tbi\" parameters is below the recommended value.\nPlease consider increasing \"local_search_evals\"...\n");
+				else
+					localEvals = localRecEvals;
+			}
+
+			printf("\nStarting local search...\n");
+
 			//	setting up local search params
 			ms_trees = localTrees;
 			evalCount = 0;
 			abortNLopt = false;
 			currState = LOCAL;
 
-			local_opt = nlopt::opt(nlopt::LN_SBPLX, tbiMsCmdIdx.size());
-			if (!localEvals) {
-				if (!skipGlobalSearch)
-					localEvals = globalEvals/5;
-				else
-					localEvals = 1000 * tbiMsCmdIdx.size();
-			}
 			vector<double> localSearchPerturb;
 			for (size_t param = 0; param < lowerBounds.size(); param++)
 				localSearchPerturb.push_back((upperBounds[param]-lowerBounds[param])/4);
+
+			local_opt = nlopt::opt(nlopt::LN_SBPLX, tbiMsCmdIdx.size());
 //			local_opt.set_xtol_rel(1e-2);
 			local_opt.set_ftol_abs(localSearchAbsTol);
 			local_opt.set_initial_step(localSearchPerturb);
